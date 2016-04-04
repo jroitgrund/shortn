16c200285f5bbb37a60d44ae5ee05c3cf41d3f4e
Jonathan Roitgrund <jroitgrund@gmail.com>
2016-04-04 18:12:48 +0100

    Working API.

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,9 +1,26 @@
 class UrlsController < ApplicationController
+
+  URLS_CACHE_PREFIX = 'urls_cache'
+  KEYS_CACHE_PREFIX = 'keys_cache'
+  SEPARATOR = '.'
+
   def create
-    render json: {}
+    url_cache_key = "#{URLS_CACHE_PREFIX}#{SEPARATOR}#{params[:url]}"
+    key = Rails.cache.read url_cache_key
+    if key.nil?
+      key = (0...10).map { (65 + rand(26)).chr }.join
+      Rails.cache.write url_cache_key, key
+      Rails.cache.write "#{KEYS_CACHE_PREFIX}#{SEPARATOR}#{key}", params[:url]
+    end
+    render json: {key: key}
   end
 
   def show
-    render json: {}
+    url = Rails.cache.read "#{KEYS_CACHE_PREFIX}#{SEPARATOR}#{params[:key]}"
+    if url.nil?
+      raise ActionController::RoutingError.new('Not Found')
+    else
+      redirect_to url
+    end
   end
 end
--- test/controllers/urls_controller_test.rb
+++ test/controllers/urls_controller_test.rb
@@ -5,8 +5,14 @@ class UrlsControllerTest < ActionController::TestCase
     url = 'http://foo.com'
     key = JSON.parse((post :create, url: url).body)['key']
     assert_response :success
-    get :show, key: 5
+    get :show, key: key
     assert_redirected_to url
   end
 
+  test "the same URL posted twice returns the same key" do
+    url = 'http://foo.com'
+    key1 = JSON.parse((post :create, url: url).body)['key']
+    key2 = JSON.parse((post :create, url: url).body)['key']
+    assert_equal key1, key2
+  end
 end
